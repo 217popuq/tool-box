b839ac8a87e7dfa12e44fdf4e0605b3f7d94bc54
Taichi Miyamoto <217popuq@gmail.com>
2025-12-18 08:39:22 +0000

    fix shell

--- wsl/wsl-initial.sh
+++ wsl/wsl-initial.sh
@@ -12,13 +12,40 @@ while true; do
   esac
 done
 
+# apt-update
+apt-get update
+
 # git install
 apt-get install git
 
 # git config
+cd ~/
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
 
 echo "Git profile set:"
 echo "  Name : $GIT_USER_NAME"
 echo "  Email: $GIT_USER_EMAIL"
+
+# azure cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+  gpg --dearmor | tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+chmod go+r /etc/apt/keyrings/microsoft.gpg
+
+AZ_DIST=$(lsb_release -cs)
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/azure-cli/
+Suites: ${AZ_DIST}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /etc/apt/keyrings/microsoft.gpg" | tee /etc/apt/sources.list.d/azure-cli.sources
+
+apt-get install azure-cli
+
+# make directory
+
+cd ~/
+mkdir .kube
